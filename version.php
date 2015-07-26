29aa2545872376ae46606e273b7d13bc3ea4c114
Daniel Neis Araujo <danielneis@gmail.com>
2015-07-26 11:34:03 -0300

    adding plugin maturity and release

--- version.php
+++ version.php
@@ -26,3 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2015051100;
 $plugin->requires = 2015050500;
 $plugin->component = 'availability_maxviews';
+$plugin->release = 1;
+$plugin->maturity   = MATURITY_RC;
