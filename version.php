a30da487fbeb32cd3c0b7b04396525f45aabe899
Daniel Neis Araujo <danielneis@gmail.com>
2018-02-06 15:35:09 -0200

    bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016060800;
+$plugin->version = 2018020600;
 $plugin->requires = 2014111000;
 $plugin->component = 'availability_maxviews';
-$plugin->release = 4;
+$plugin->release = 5;
 $plugin->maturity   = MATURITY_STABLE;
