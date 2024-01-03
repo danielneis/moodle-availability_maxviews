cc607d43186d33ac8d03931b1c5d86c818fc357b
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2024-01-03 17:41:36 +0200

    use observer instate of time interval

--- amd/src/display.js
+++ amd/src/display.js
@@ -21,38 +21,51 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 import $ from "jquery";
-// import ajax from "core/ajax";
 
+/**
+ *
+ * @param {number} cmid the cm id
+ * @param {string} render the content
+ */
 export const init = (cmid, render) => {
-    // The following listener is needed for the Tiles course format, where sections are loaded on demand.
-    // $(document).ajaxComplete(function(event, xhr, settings) {
-    //     if (typeof (settings.data) !== 'undefined') {
-    //         var data = JSON.parse(settings.data);
-    //         if (data.length > 0 && typeof (data[0].methodname) !== 'undefined') {
-    //             if (data[0].methodname == 'format_tiles_get_single_section_page_html' // Tile load.
-    //                 || data[0].methodname == 'format_tiles_log_tile_click'
-    //                 ) { // Tile load, cached.
-    //                 $(document).ready(function() {
-    //                     var existed = $("#availability_maxviews_count_" + cmid);
-    //                     if (!existed || !existed[0] || existed[0] == undefined) {
-    //                         var module = $("#module-"+cmid+" .description");
-    //                         module.append(render);
-    //                     }
-    //                 });
-    //             }
-    //         }
-    //     }
-    // });
+
     // For any other course format.
-    $(document).ready(function() {
-        setInterval(function() {
-            var existed = $("#availability_maxviews_count_"+cmid);
-            if (!existed || !existed[0] || existed[0] == undefined) {
-                var module = $("#module-"+cmid+" .description");
-                if (module !== null) {
-                    module.append(render);
-                }
+    // Some course formats like Tiles uses Ajax to display course modules.
+    // Use mutation observers to observe any change in the main region
+    // to display the required information.
+    const config = { attributes: true, childList: true, subtree: true };
+
+    const callback = (mutationList) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList") {
+                appendViews(cmid, render);
             }
-        }, 500);
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    $(document).ready(function() {
+        let regionMain = document.getElementById("region-main");
+        appendViews(cmid, render);
+        // Start observing the target node for configured mutations
+        observer.observe(regionMain, config);
     });
 };
+
+/**
+ * append information about limitation of max number allowed to view a course module.
+ *
+ * @param {number} cmid
+ * @param {string} render
+ */
+function appendViews(cmid, render) {
+    let existed = $("#availability_maxviews_count_"+cmid);
+    if (!existed || !existed[0] || existed[0] == undefined) {
+        let module = $("#module-"+cmid+" .description");
+        if (module !== null) {
+            module.append(render);
+        }
+    }
+}
